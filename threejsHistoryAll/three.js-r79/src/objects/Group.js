ccf9007a4d861c1b84af14d3179a4d7bfdb03eda
dibashi <dibashi1989@163.com>
2019-04-29 22:51:39 +0800

    Update Group.js

--- threejsHistoryAll/three.js-r79/src/objects/Group.js
+++ threejsHistoryAll/three.js-r79/src/objects/Group.js
@@ -1,7 +1,11 @@
 /**
  * @author mrdoob / http://mrdoob.com/
  */
-
+/**
+ * This is almost identical to an Object3D. 
+ * Its purpose is to make working with groups of objects syntactically clearer.
+ */
+//搜索过引擎了，没有任何特殊处理，就是一个Object3D 目的就是为了清晰表达意思
 THREE.Group = function () {
 
 	THREE.Object3D.call( this );
